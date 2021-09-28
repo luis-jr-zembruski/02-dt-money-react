5ea061572ef43055725d4c75f11cd97d8a200d6d
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2021-09-28 12:52:37 -0300

    Isolando o Modal em um Componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
-Modal.setAppElement("#root");
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -20,13 +19,14 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
-      <Modal
+
+      <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Informação</h2>
-      </Modal>
+      />
+      
       <GlobalStyle />
     </>
   );
