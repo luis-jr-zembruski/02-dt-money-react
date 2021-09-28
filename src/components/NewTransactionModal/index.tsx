4164cc03b9671ec30bf0687a0353b24f658b16f4
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2021-09-28 13:27:27 -0300

    estilização prévia do Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -10,15 +10,36 @@ Modal.setAppElement("#root");
 
 export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionModalProps) {
   return (
-    <Container>
-      
+    
       <Modal
         isOpen={isOpen}
         onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
       >
-        <h2>Cadastrar Informação</h2>
+
+        <Container>
+          <h2>Cadastrar transação</h2>
+
+          <input 
+            placeholder="Título"
+          />
+
+          <input 
+            type="number"
+            placeholder="Valor"
+          />
+
+          <input 
+            placeholder="Categoria"
+          />
+
+          <button type="submit">
+            Cadastrar
+          </button>
+        </Container>
+
       </Modal>
 
-    </Container>
   );
 }
